608eb20d19d394d9da203ba0490572a39b421638
SoeSuNwe <ssnwe@galaxywave.com>
2023-09-24 20:59:12 +0630

    init project

---
+++ AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager2
+{ 
+    public class AppDbContext : DbContext
+    {
+         
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager2.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
